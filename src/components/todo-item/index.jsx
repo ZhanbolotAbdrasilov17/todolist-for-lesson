e48dc9815965caafdb687c3be9cd02949638df24
ZhanbolotAbdrasilov17 <itpythonzhanbolot@gmail.com>
2023-03-28 20:09:59 +0600

    second commit

--- src/components/todo-item/index.jsx
+++ src/components/todo-item/index.jsx
@@ -1,16 +1,23 @@
+import styled from "styled-components"
+
+const TodoContainer = styled.li`
+  list-style: none;
+  background-color: #ccc;
+`
+
 export const TodoItem = ({ id, text, isDone, onToggleTodo }) => {
   const handleToggleTodo = () => {
     onToggleTodo?.(id)
   }
 
   return (
-    <li>
+    <TodoContainer>
       <input
         type="checkbox"
         checked={isDone}
         onChange={handleToggleTodo}
       />
       <span style={{ textDecoration: isDone ? 'line-through' : 'none' }}>{text}</span>
-    </li>
+    </TodoContainer>
   )
 }
